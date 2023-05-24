20853d2549ad4e81801d1e4185cdd65cc4ae37ab
nyvnge <sandersonnyange@gmail.com>
2023-05-24 23:36:05 +0300

    C++ sum of two numbers

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+using namespace std;
+
+int main(){
+	int fnumber, snumber, sum;
+
+	cout << "Enter the two numbers";
+	cin >> fnumber >> snumber;
+
+	sum = fnumber + snumber;
+	cout << sum;
+
+	return 0;
+}
